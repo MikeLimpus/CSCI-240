eccc61b116f3c4f40caec421eae42124fcafd6c0
ml <ml@mike-server.mikelimpus.com>
2018-08-17 22:34:05 -0700

    Homework 1

--- Homework/Homework 1/Problem1.c
+++ Homework/Homework 1/Problem1.c
@@ -2,16 +2,21 @@
  * Jonathan Limpus
  * Problem 1
  * Problem: input decimal integer, and output same integer in hexadecimal form 
+ * Solution: Create an integer variable named 'convert', use scanf() to have 
+ * user input a value for 'convert', and output the same number as hexadecimal
+ * using %x 
  */
 #include <stdio.h>
 
 int main() {
 	int convert = 0;	//Integer value to be converted to hex
-	for(int i = 0; i < 3; ++i) {
-		printf("----- TEST CASE %d -----\n", i);
+	 
+	for(int i = 0; i < 3; ++i) {	
+		//create 3 test cases to show different values being converted
+		printf("\n----- TEST CASE %d -----\n", i);
 		printf("Please enter an integer to be converted to hexadecimal: ");
 		scanf("%d", &convert);
-		printf("The decimal integer %d is %x in hexadecimal\n", convert, convert);
+		printf("The decimal integer '%d' is '%x' in hexadecimal\n", convert, convert);
 	}
 	return 0;
 }
