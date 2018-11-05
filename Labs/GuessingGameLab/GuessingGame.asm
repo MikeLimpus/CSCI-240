abb6570d122d6178bd98b4ecc498f3913b1f31d3
U-CIS\Student <Student@O3240-121-12.cis.craftonhills.edu>
2018-11-05 15:15:57 -0800

    lab work

--- Labs/GuessingGameLab/GuessingGame.asm
+++ Labs/GuessingGameLab/GuessingGame.asm
@@ -10,6 +10,7 @@
 ; Read character from the console, if not equal to '\n' then save the
 ; character to memory; otherwise, terminate
 ;
+			
 ReadChar	IN
 ; Test if character is '\n' (x000A)
 			ADD R2, R0, #-10
@@ -21,11 +22,16 @@ ReadChar	IN
 ; Append the null character '\0' (0x0000) to the string and output
 ; the string to the console
 Terminate  	AND R0, R0, #0 ; Zero out register
-			LDR R1, R0, #0 ; Write '\0' (0x0000) to memory
-			LD R0, PTR     ; Load string base pointer to R0
+			STR R1, R0, #0 ; Write '\0' (0x0000) to memory
+			LEA R0, PTR    ; Load string base pointer to R0
 			PUTS           ; Print string found at R0
 			HALT
 ; PTR is the base pointer to the stored string
 NEWLINE    .FILL x00A0
 PTR        .FILL x4000
+PROMPT	   .STRINGZ "Guess a number between 0 & 9" 
+SMALL	   .STRINGZ "Too small" 
+BIG 	   .STRINGZ "Too Big" 
+CORRECT	   .STRINGZ "Correct!"
 .END
+
