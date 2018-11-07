12f3edb3286183eaa4ad29da0115566a1a914883
Mike Limpus <mikelimpus111@gmail.com>
2018-11-07 09:57:28 -0800

    h

--- Projects/Project1/Problem1.asm
+++ Projects/Project1/Problem1.asm
@@ -5,12 +5,12 @@
 		.ORIG x3000
 		LD R0 BIN
 		LD R1 INT
-LOOP	ADD R0 R0 R0
+LOOP		ADD R0 R0 R0
 		ADD R1 R1 -1 
 		BRp LOOP 
 		ST R0 SHIFT
-		HALT 
-		;.ORIG x3100
+		HALT
+
 BIN		.FILL xFFFA	; Binary string b 
 INT		.FILL x0004	; Integer n, must be in range[0, 16]
 SHIFT	.FILL x0000	; Clear memory location x3102 
