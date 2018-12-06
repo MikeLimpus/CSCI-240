3bc17f31e42476fb09fb4a64b8e1118b546976e9
MikeLimpus <mikelimpus111@gmail.com>
2018-12-06 11:50:58 -0800

    old version of project 2

--- Projects/Project2/Project2.asm
+++ Projects/Project2/Project2.asm
@@ -29,15 +29,50 @@ End     HALT
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 Encrypt ST R7 ER7       ; Save R7(PC) from PUTS
         AND R0 R0 0     ; Clear registers used in main function
+        AND R4 R4 0
         LEA R0 EPrompt
         PUTS 
         AND R0 R0 0     ; Clear R0
         GETC            ; Get digit (no input validation) 
         LD R1 ASCII
-        ADD R0 R0 ASCII ; Convert input char to int 
+        ADD R4 R0 R1    ; Convert input char to int and store in new register
         STI R0 KeyPtr   ; Store input at x4001 
+        OUT             ; Print key 
         AND R0 R0 0     ; Clear R0 
-        LD R7 ER7       ; Load R7 with former PC 
+        LEA R0 SPrompt  ; Load prompt for input 
+        PUTS
+        AND R0 R0 0     ; Clear R0 
+                        ; Begin inputting 20 chars 
+        LD R3 StrPtr    ; Pointer to where the input should be stored 
+        LD R5 SolPtr    ; Pointer to output
+        ADD R2 R2 10    ; Counter = 20
+        ADD R2 R2 10
+EInput  IN 
+        ; Check if \n was inputted 
+        ADD R0 R0 -10   ; Newline == ASCII 10
+        BRz EIend       ; Break if input == \n 
+        ADD R0 R0 10    ; Else, return input back to what it was 
+        STR R0 R3 0     ; Store character in memory x4002 + n 
+; Perform encryption algorithm                        
+; xxxx xxxx xxxx xxxx AND 0000 0000 0000 0001 = 0 if LSB is 0
+; Thus, if (input AND 1) = 0, add 1, then add key 
+; else subract 1, and add key 
+; Store location in memory
+        AND R1 R0 1      
+        BRz EZero
+        BRp EOne
+EZero   ADD R1 R1 1     ; Toggle LSB 
+        BRnzp ENext 
+EOne    ADD R1 R1 -1    ; Toggle LSB 
+ENext   ADD R1 R1 R4    ; Key is in R4 
+        STR R1 R5 0     ; Store at x4020 + n 
+        ADD R3 R3 1     ; Increment pointers &
+        ADD R5 R5 1
+        ADD R2 R2 -1    ; Decrement counter
+        BRp EInput
+        
+        ;STI R0 StrPtr   ; Store string at x4002 
+EIend   LD R7 ER7       ; Load R7 with former PC 
         RET
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
 Decrypt ST R7 DR7       ; Save R7(PC) from PUTS
@@ -51,9 +86,10 @@ Decrypt ST R7 DR7       ; Save R7(PC) from PUTS
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 Prompt  .STRINGZ "(E)ncrypt or (D)ecrypt\n"
-InFail  .STRINGZ "Illegal Input. Try again.\n
+InFail  .STRINGZ "Illegal Input. Try again.\n"
 EPrompt .STRINGZ "Please input your single-digit encryption key\n"
 DPrompt .STRINGZ "Please input your single-digit decryption key\n"
+SPrompt .STRINGZ "Please begin inputting your string (20 char limit)"
 NegD    .FILL -68
 ASCII   .FILL -48
 InPtr   .FILL x4000
